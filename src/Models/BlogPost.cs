b0b687bfaaf7a3418a98964a1fdcd4faf8817a14
neilmarshall <neil.marshall@dunelm.org.uk>
2024-11-10 22:29:34 +0000

    add 'Models' class library

---
+++ src/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace Models;
+
+public class BlogPost
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+    public DateTime PublishedDate { get; set; }
+    public string[] Tags { get; set; } = [];
+    public required string BlogPostMarkdown { get; set; }
+    public bool PreviewIsComplete { get; set; }
+}
