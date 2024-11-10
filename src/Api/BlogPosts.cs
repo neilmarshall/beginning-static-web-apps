4079e45677e47bede35a7f085a4464e7f31c56c8
neilmarshall <neil.marshall@dunelm.org.uk>
2024-11-10 23:04:25 +0000

    add BlogPosts Http-triggered function

---
+++ src/Api/BlogPosts.cs
@@ -0,0 +1,50 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace Api;
+
+public class BlogPosts(ILogger<BlogPosts> logger)
+{
+    // public static object UriFactory { get; private set; }
+
+    [Function($"{nameof(BlogPosts)}_Get")]
+    public IActionResult GetAllBlogPosts(
+        [HttpTrigger( AuthorizationLevel.Anonymous, "get", Route = "blogposts")] HttpRequest req,
+        [CosmosDBInput(
+            "SwaBlog",
+            "BlogContainer",
+            Connection = "CosmosDbConnectionString",
+            SqlQuery = @"
+                SELECT c.id
+                    ,c.Title
+                    ,c.Author
+                    ,c.PublishedDate
+                    ,LEFT(c.BlogpostMarkdown, 500) AS BlogPostMarkdown
+                    ,LENGTH(c.BlogpostMarkdown) <= 500 AS PreviewIsComplete
+                    ,c.Tags
+                FROM c
+                WHERE c.Status = 2")] IEnumerable<BlogPost> blogPosts)
+    {
+        logger.LogInformation("Hello Mummy");
+        return new OkObjectResult(blogPosts);
+    }
+
+    [Function($"{nameof(BlogPosts)}_GetId")]
+    public IActionResult GetBlogPost(
+        [HttpTrigger( AuthorizationLevel.Anonymous, "get", Route = "blogposts/{author}/{id}")] HttpRequest req,
+        [CosmosDBInput(
+            "SwaBlog",
+            "BlogContainer",
+            Connection = "CosmosDbConnectionString",
+            PartitionKey = "{author}",
+            Id = "{id}")] BlogPost blogPost)
+    {
+        if (blogPost is null)
+            return new NotFoundResult();
+
+        return new OkObjectResult(blogPost);
+    }
+}
