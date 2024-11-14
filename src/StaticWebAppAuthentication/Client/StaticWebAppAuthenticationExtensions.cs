0f514a9a32de19f7795865aca7b805825eda8a0a
neilmarshall <neil.marshall@dunelm.org.uk>
2024-11-14 22:36:22 +0000

    add authn/authz

---
+++ src/StaticWebAppAuthentication/Client/StaticWebAppAuthenticationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StaticWebAppAuthentication.Client;
+
+public static class StaticWebAppAuthenticationExtensions
+{
+    public static IServiceCollection AddStaticWebsAuthentication(this IServiceCollection services) =>
+        services
+            .AddAuthorizationCore()
+            .AddScoped<AuthenticationStateProvider, StaticWebAppsAuthenticationStateProvider>();
+}
---
+++ src/StaticWebAppAuthentication/Models/ClientPrincipal.cs
@@ -0,0 +1,9 @@
+﻿namespace StaticWebAppAuthentication.Models;
+
+public record class ClientPrincipal
+{
+    public string IdentityProvider { get; set;} = string.Empty;
+    public string UserId { get; set;} = string.Empty;
+    public string UserDetails { get; set;} = string.Empty;
+    public IEnumerable<string> UserRoles { get; set;} = Enumerable.Empty<string>();
+}
