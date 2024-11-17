2f890ad3f038b99130f491a5bdd40a94b2a19a85
neilmarshall <neil.marshall@dunelm.org.uk>
2024-11-17 20:54:13 +0000

    simplify initialization expression in ClientPrincipal.cs

--- src/StaticWebAppAuthentication/Models/ClientPrincipal.cs
+++ src/StaticWebAppAuthentication/Models/ClientPrincipal.cs
@@ -5,5 +5,5 @@ public record class ClientPrincipal
     public string IdentityProvider { get; set;} = string.Empty;
     public string UserId { get; set;} = string.Empty;
     public string UserDetails { get; set;} = string.Empty;
-    public IEnumerable<string> UserRoles { get; set;} = Enumerable.Empty<string>();
+    public IEnumerable<string> UserRoles { get; set;} = [];
 }
